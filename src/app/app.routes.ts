ed2738eb8be0a174807d8b684b26c848e9546547
Steve Fulghum <steve@fulghum.digital>
2025-08-08 20:38:44 -0400

    Add basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Page } from './components/page/page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'resume' },
+  { path: 'resume', component: Page },
+  { path: '**', redirectTo: 'resume' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Page } from './components/page/page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Page],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
